3a994893840552e36369a51804c78972aad44065
Viraj.Jage <viraj@safexpay.com>
2020-03-05 17:47:32 +0530

    Initial commit

---
+++ app/src/main/java/com/virajjage/ecommerceapp/DashBoardActivity.kt
@@ -0,0 +1,12 @@
+package com.virajjage.ecommerceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashBoardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
